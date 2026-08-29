141d7cc0d593764e2f4c428bb5ea769d59f6c4a4
wealth Eniamo <patweadan@gmail.com>
2026-08-29 06:02:12 +0100

    chore: initialize blockchain payment API

---
+++ app/db/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "sqlite:///./blockchain.db"
+
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
